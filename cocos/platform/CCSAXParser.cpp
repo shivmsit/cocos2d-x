ccf7f37030189e3dfc81e668095d5fcdde6bc6c7
Shivpratap Chauhan <shivmsit@gmail.com>
2026-07-27 18:47:16 +0530

    Replace RapidXML SAX path with TinyXML2

--- cocos/platform/CCSAXParser.cpp
+++ cocos/platform/CCSAXParser.cpp
@@ -29,7 +29,6 @@
 
 #include "platform/CCFileUtils.h"
 #include "tinyxml2.h"
-#include "rapidxml/rapidxml_sax3.hpp"
 
 NS_CC_BEGIN
 
@@ -88,37 +87,6 @@ bool XmlSaxHander::Visit( const tinyxml2::XMLText& text )
     return true;
 }
 
-/// rapidxml SAX handler
-class RapidXmlSaxHander : public rapidxml::xml_sax2_handler
-{
-
-public:
-    RapidXmlSaxHander() :_ccsaxParserImp(0) {};
-
-    void setSAXParserImp(SAXParser* parser)
-    {
-        _ccsaxParserImp = parser;
-    }
-
-    void xmlSAX2StartElement(const char *name, size_t /*len*/, const char **atts, size_t /*attslen*/) override
-    {
-        SAXParser::startElement(_ccsaxParserImp, (const CC_XML_CHAR *)name, (const CC_XML_CHAR **)atts);
-    }
-
-    void xmlSAX2EndElement(const char *name, size_t /*len*/) override
-    {
-        SAXParser::endElement(_ccsaxParserImp, (const CC_XML_CHAR *)name);
-    }
-
-    void xmlSAX2Text(const char *s, size_t len) override
-    {
-        SAXParser::textHandler(_ccsaxParserImp, (const CC_XML_CHAR *)s, len);
-    }
-
-private:
-    SAXParser *_ccsaxParserImp;
-};
-
 SAXParser::SAXParser()
 {
     _delegator = nullptr;
@@ -158,21 +126,7 @@ bool SAXParser::parse(const std::string& filename)
 
 bool SAXParser::parseIntrusive(char* xmlData, size_t dataLength)
 {
-    RapidXmlSaxHander printer;
-    printer.setSAXParserImp(this);
-
-    rapidxml::xml_sax3_parser<> parser(&printer);
-    try {
-        parser.parse<>(xmlData, static_cast<int>(dataLength));
-        return true;
-    }
-    catch (rapidxml::parse_error& e)
-    {
-        CCLOG("cocos2d: SAXParser: Error parsing xml: %s at %s", e.what(), e.where<char>());
-        return false;
-    }
-
-    return false;
+    return parse(xmlData, dataLength);
 }
 
 void SAXParser::startElement(void *ctx, const CC_XML_CHAR *name, const CC_XML_CHAR **atts)
@@ -194,5 +148,4 @@ void SAXParser::setDelegator(SAXDelegator* delegator)
 }
 
 NS_CC_END
-
 
