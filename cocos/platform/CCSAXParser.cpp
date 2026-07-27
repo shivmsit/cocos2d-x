4859772409359c7c43aa8b27af743dc1d213ae15
Shivpratap Chauhan <shivmsit@gmail.com>
2026-07-27 18:47:16 +0530

    Replace RapidXML SAX path with TinyXML2

--- cocos/platform/CCSAXParser.cpp
+++ cocos/platform/CCSAXParser.cpp
@@ -28,40 +28,63 @@
 #include <vector> // because its based on windows 8 build :P
 
 #include "platform/CCFileUtils.h"
-#include "rapidxml/rapidxml_sax3.hpp"
+#include "tinyxml2.h"
 
 NS_CC_BEGIN
 
-/// rapidxml SAX handler
-class RapidXmlSaxHander : public rapidxml::xml_sax2_handler
+class XmlSaxHander : public tinyxml2::XMLVisitor
 {
-
 public:
-    RapidXmlSaxHander() :_ccsaxParserImp(0) {};
+    XmlSaxHander():_ccsaxParserImp(0){};
+
+    virtual bool VisitEnter( const tinyxml2::XMLElement& element, const tinyxml2::XMLAttribute* firstAttribute );
+    virtual bool VisitExit( const tinyxml2::XMLElement& element );
+    virtual bool Visit( const tinyxml2::XMLText& text );
+    virtual bool Visit( const tinyxml2::XMLUnknown&){ return true; }
 
     void setSAXParserImp(SAXParser* parser)
     {
         _ccsaxParserImp = parser;
     }
 
-    void xmlSAX2StartElement(const char *name, size_t /*len*/, const char **atts, size_t /*attslen*/) override
-    {
-        SAXParser::startElement(_ccsaxParserImp, (const CC_XML_CHAR *)name, (const CC_XML_CHAR **)atts);
-    }
-
-    void xmlSAX2EndElement(const char *name, size_t /*len*/) override
-    {
-        SAXParser::endElement(_ccsaxParserImp, (const CC_XML_CHAR *)name);
-    }
-
-    void xmlSAX2Text(const char *s, size_t len) override
-    {
-        SAXParser::textHandler(_ccsaxParserImp, (const CC_XML_CHAR *)s, len);
-    }
-
 private:
     SAXParser *_ccsaxParserImp;
 };
+
+bool XmlSaxHander::VisitEnter( const tinyxml2::XMLElement& element, const tinyxml2::XMLAttribute* firstAttribute )
+{
+    //log(" VisitEnter %s",element.Value());
+
+    std::vector<const char*> attsVector;
+    for( const tinyxml2::XMLAttribute* attrib = firstAttribute; attrib; attrib = attrib->Next() )
+    {
+        //log("%s", attrib->Name());
+        attsVector.push_back(attrib->Name());
+        //log("%s",attrib->Value());
+        attsVector.push_back(attrib->Value());
+    }
+
+    // nullptr is used in c++11
+    //attsVector.push_back(nullptr);
+    attsVector.push_back(nullptr);
+
+    SAXParser::startElement(_ccsaxParserImp, (const CC_XML_CHAR *)element.Value(), (const CC_XML_CHAR **)(&attsVector[0]));
+    return true;
+}
+bool XmlSaxHander::VisitExit( const tinyxml2::XMLElement& element )
+{
+    //log("VisitExit %s",element.Value());
+
+    SAXParser::endElement(_ccsaxParserImp, (const CC_XML_CHAR *)element.Value());
+    return true;
+}
+
+bool XmlSaxHander::Visit( const tinyxml2::XMLText& text )
+{
+    //log("Visit %s",text.Value());
+    SAXParser::textHandler(_ccsaxParserImp, (const CC_XML_CHAR *)text.Value(), strlen(text.Value()));
+    return true;
+}
 
 SAXParser::SAXParser()
 {
@@ -80,11 +103,12 @@ bool SAXParser::init(const char* /*encoding*/)
 
 bool SAXParser::parse(const char* xmlData, size_t dataLength)
 {
-    if(xmlData != nullptr && dataLength > 0) {
-        std::string mutableData(xmlData, dataLength);
-        return this->parseIntrusive(&mutableData.front(), dataLength);
-    }
-    return false;
+    tinyxml2::XMLDocument tinyDoc;
+    tinyDoc.Parse(xmlData, dataLength);
+    XmlSaxHander printer;
+    printer.setSAXParserImp(this);
+
+    return tinyDoc.Accept(&printer);
 }
 
 bool SAXParser::parse(const std::string& filename)
@@ -93,7 +117,7 @@ bool SAXParser::parse(const std::string& filename)
     Data data = FileUtils::getInstance()->getDataFromFile(filename);
     if (!data.isNull())
     {
-        ret = parseIntrusive((char*)data.getBytes(), data.getSize());
+        ret = parse((const char*)data.getBytes(), data.getSize());
     }
 
     return ret;
@@ -101,21 +125,7 @@ bool SAXParser::parse(const std::string& filename)
 
 bool SAXParser::parseIntrusive(char* xmlData, size_t dataLength)
 {
-    RapidXmlSaxHander printer;
-    printer.setSAXParserImp(this);
-
-    rapidxml::xml_sax3_parser<> parser(&printer);
-    try {
-        parser.parse<>(xmlData, static_cast<int>(dataLength));
-        return true;
-    }
-    catch (rapidxml::parse_error& e)
-    {
-        CCLOG("cocos2d: SAXParser: Error parsing xml: %s at %s", e.what(), e.where<char>());
-        return false;
-    }
-
-    return false;
+    return parse(xmlData, dataLength);
 }
 
 void SAXParser::startElement(void *ctx, const CC_XML_CHAR *name, const CC_XML_CHAR **atts)
@@ -137,5 +147,3 @@ void SAXParser::setDelegator(SAXDelegator* delegator)
 }
 
 NS_CC_END
-
-
