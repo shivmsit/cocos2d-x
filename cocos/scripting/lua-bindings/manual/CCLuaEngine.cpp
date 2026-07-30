cdd56ca16d9e457e067c759cf09e7236e25c0add
Shivpratap Chauhan <shivmsit@gmail.com>
2026-07-30 18:17:57 +0530

    Build the Lua runtime stack from source

--- cocos/scripting/lua-bindings/manual/CCLuaEngine.cpp
+++ cocos/scripting/lua-bindings/manual/CCLuaEngine.cpp
@@ -36,7 +36,9 @@
 #include "base/CCDirector.h"
 #include "base/CCEventCustom.h"
 
-#pragma comment(lib,"lua51.lib")
+#if defined(_MSC_VER)
+#pragma comment(lib, "lua51.lib")
+#endif
 
 NS_CC_BEGIN
 
