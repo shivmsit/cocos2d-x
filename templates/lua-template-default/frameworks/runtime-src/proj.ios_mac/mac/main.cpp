1f34ae6f0251efdc7e7bb4c4c688c5672445ba6b
Shivpratap Chauhan <shivmsit@gmail.com>
2026-07-30 19:19:22 +0530

    Add license to Lua template launcher

--- templates/lua-template-default/frameworks/runtime-src/proj.ios_mac/mac/main.cpp
+++ templates/lua-template-default/frameworks/runtime-src/proj.ios_mac/mac/main.cpp
@@ -1,12 +1,13 @@
+// Copyright (c) 2026 Shivpratap Chauhan <shivmsit@gmail.com>
+// SPDX-License-Identifier: MIT
 
 #include "AppDelegate.h"
 #include "cocos2d.h"
 
 USING_NS_CC;
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     AppDelegate app;
     return Application::getInstance()->run();
 }
-
